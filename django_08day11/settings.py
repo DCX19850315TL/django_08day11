4d06af6a6dd170e78a5e7ffdfb83cd58664b7132
tanglei <tanglei_0315@163.com>
2017-11-28 18:04:53 +0800

    练习编写运维平台

--- django_08day11/settings.py
+++ django_08day11/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'django_08day11.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':'08day11',
+    'USER': 'root',
+    'PASSWORD': '123456',
+    'HOST': '',
+    'PORT': '',
+    'OPTIONS':{
+        'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+    },
     }
 }
 
--- web/models.py
+++ web/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+
+    username = models.CharField(max_length=50)
+
+    password = models.CharField(max_length=50)
+
